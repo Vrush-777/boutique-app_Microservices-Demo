299bb068ed4d2951d3b1e3cd758e1148ac6317a7
vrush-777 <vrush.raskar777@gmail.com>
2026-08-03 13:09:27 +0530

    Add AGIC role assignments

--- terraform/environments/dev/agic.tf
+++ terraform/environments/dev/agic.tf
@@ -1,11 +1,23 @@
 resource "azurerm_role_assignment" "agic_appgw_contributor" {
+
   scope                = module.application_gateway.application_gateway_id
   role_definition_name = "Contributor"
-  principal_id         = module.aks.agic_identity_object_id
+
+  principal_id = module.aks.agic_identity_object_id
+
+  depends_on = [
+    module.aks
+  ]
 }
 
 resource "azurerm_role_assignment" "agic_rg_reader" {
+
   scope                = module.resource_group.resource_group_id
   role_definition_name = "Reader"
-  principal_id         = module.aks.agic_identity_object_id
+
+  principal_id = module.aks.agic_identity_object_id
+
+  depends_on = [
+    module.aks
+  ]
 }
--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -138,21 +138,3 @@ module "aks" {
     ManagedBy   = "Terraform"
   }
 }
-
-resource "azurerm_role_assignment" "agic_reader" {
-  scope = module.resource_group.resource_group_id
-  role_definition_name = "Reader"
-  principal_id = module.aks.agic_identity_object_id
-  depends_on = [
-    module.aks
-    ]
-}
-
-resource "azurerm_role_assignment" "agic_contributor" {
-  scope = module.application_gateway.application_gateway_id
-  role_definition_name = "Contributor"
-  principal_id = module.aks.agic_identity_object_id
-  depends_on = [
-    module.aks
-    ]
-}
