5c5c50e276ffc618435e0ee91465b01233c08a3f
vrush-777 <vrush.raskar777@gmail.com>
2026-08-04 14:48:17 +0530

    test terraform automation

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -81,11 +81,6 @@ variable "max_count" {
   type = number
 }
 
-# variable "identity_name" {
-#   description = "User Assigned Managed Identity Name"
-#   type        = string
-# }
-
 variable "application_gateway_capacity" {
   description = "Application Gateway capacity"
   type        = number
